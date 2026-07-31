9d4125fb32bdb2eda4716e7b38074710e1d03819
sawon-21 <businessmansawon@gmail.com>
2026-07-31 19:29:58 +0600

    Change date range from 7 days to 3 days

--- filter.js
+++ filter.js
@@ -51,7 +51,7 @@ export function isNoticeNew(item) {
   const now = new Date();
   const diffMs = now.getTime() - d.getTime();
   const diffDays = diffMs / (1000 * 60 * 60 * 24);
-  return diffDays >= 0 && diffDays <= 7;
+  return diffDays >= 0 && diffDays <= 3;
 }
 
 export function getDeptIcon(dept) {
